039cda7242be8c356563f4e80e6868de1e2ddd76
Lisa Jenkins <lisa@devpipline.com>
2022-10-17 19:54:02 -0600

    first challenge

--- strings.js
+++ strings.js
@@ -1,1 +1,8 @@
+/*1) Use startsWith and endsWith methods on the provided string, and have them both return as true:
+const string = "Why are we here???"; */
+
 const string = "Why are we here???";
+
+console.log(string.startsWith("Why are we here???"));
+
+console.log(string.endsWith("Why are we here???"));
